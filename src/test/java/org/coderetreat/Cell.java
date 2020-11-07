0d05d30a283536678ab11bca1b70d7741ad1a581
sfauvel <sfauvel@gmail.com>
2020-11-07 14:01:49 +0100

    Refactor test

--- src/test/java/org/coderetreat/Cell.java
+++ src/test/java/org/coderetreat/Cell.java
@@ -5,6 +5,9 @@ public enum Cell {
     Alive, Dead;
 
     public Cell evolve(int aliveNeighbours) {
+        if(aliveNeighbours>=2) {
+            return Alive;
+        }
         return Dead;
     }
 
--- src/test/java/org/coderetreat/CellTest.java
+++ src/test/java/org/coderetreat/CellTest.java
@@ -9,9 +9,22 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 public class CellTest {
     @Test
     public void cell_without_neighbour_should_die() {
+        assertNextState(0, Cell.Dead);
+    }
+
+    @Test
+    public void cell_with_2_neighbours_should_stay_alive() {
+        assertNextState(2, Cell.Alive);
+    }
+
+    @Test
+    public void cell_with_3_neighbours_should_stay_alive() {
+        assertNextState(3, Cell.Alive);
+    }
+
+    private void assertNextState(int aliveNeighbours, Cell expectedCell) {
         final Cell cell = Cell.Alive;
-        int aliveNeighbours = 0;
         Cell newCell = cell.evolve(aliveNeighbours);
-        assertEquals(Cell.Dead, newCell);
+        assertEquals(expectedCell, newCell);
     }
 }
