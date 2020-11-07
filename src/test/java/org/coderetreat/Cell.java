168a7917b58d0179f94c806fdbc471cd796721f0
sfauvel <sfauvel@gmail.com>
2020-11-07 14:16:29 +0100

    Create abstract method

--- src/test/java/org/coderetreat/Cell.java
+++ src/test/java/org/coderetreat/Cell.java
@@ -2,13 +2,23 @@ package org.coderetreat;
 
 public enum Cell {
 
-    Alive, Dead;
+    Alive {
+        @Override
+        public Cell evolve(int aliveNeighbours) {
+            return aliveNeighbours == 2 || aliveNeighbours == 3
+                    ? Alive
+                    : Dead;
+        }
+    },
+    Dead {
+        @Override
+        public Cell evolve(int aliveNeighbours) {
+            return Alive;
+        }
+    };
 
-    public Cell evolve(int aliveNeighbours) {
+    public abstract Cell evolve(int aliveNeighbours);
 
-        return aliveNeighbours == 2 || aliveNeighbours == 3
-                ? Alive
-                : Dead;
-    }
+
 
 }
--- src/test/java/org/coderetreat/CellTest.java
+++ src/test/java/org/coderetreat/CellTest.java
@@ -8,25 +8,33 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class CellTest {
     @Test
-    public void cell_without_neighbour_should_die() {
+    public void alive_cell_without_neighbour_should_die() {
         assertNextState(0, Cell.Dead);
     }
 
     @Test
-    public void cell_with_2_neighbours_should_stay_alive() {
+    public void alive_cell_with_2_neighbours_should_stay_alive() {
         assertNextState(2, Cell.Alive);
     }
 
     @Test
-    public void cell_with_3_neighbours_should_stay_alive() {
+    public void alive_cell_with_3_neighbours_should_stay_alive() {
         assertNextState(3, Cell.Alive);
     }
 
     @Test
-    public void cell_with_4_neighbours_should_stay_die() {
+    public void alive_cell_with_4_neighbours_should_stay_die() {
         assertNextState(4, Cell.Dead);
     }
 
+    @Test
+    public void dead_cell_with_3_alive_neighbours_should_become_alive() {
+        int aliveNeighbours = 3;
+        final Cell cell = Cell.Dead;
+        Cell newCell = cell.evolve(aliveNeighbours);
+        assertEquals(Cell.Alive, newCell);
+    }
+    
     private void assertNextState(int aliveNeighbours, Cell expectedCell) {
         final Cell cell = Cell.Alive;
         Cell newCell = cell.evolve(aliveNeighbours);
